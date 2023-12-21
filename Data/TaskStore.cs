5e21d824a794bd7a4c0479354b4de416480ed44d
hav0kinho <ruallysonfelype@gmail.com>
2023-12-21 19:11:11 -0300

    Criação da 'Base de dados' em forma de classe para as Tasks

---
+++ Data/TaskStore.cs
@@ -0,0 +1,12 @@
+﻿using TasksAPI.Models;
+
+namespace TasksAPI.Data;
+
+public static class TaskStore
+{
+    public static List<TaskModel> taskList = new List<TaskModel>
+    {
+        new TaskModel{Id=1, Title="Limpar a casa", IsCompleted=false},
+        new TaskModel{Id=2, Title="Fazer comida", IsCompleted=true},
+    };
+}
