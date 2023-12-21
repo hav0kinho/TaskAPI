e9a6b546eef03ea7cb0aa0fb0fec2a7c5b62aa3d
hav0kinho <ruallysonfelype@gmail.com>
2023-12-21 20:54:11 -0300

    Configurando os Endpoints da aplicação aparti do TaskEndpoints, utilizando o método Map().

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Swashbuckle.AspNetCore; // Biblioteca para Swagger
 
 using TasksAPI.Data;
 using TasksAPI.Models;
+using TasksAPI.Endpoints;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,16 +15,20 @@ builder.Services.AddSwaggerGen(); // Permite a geração do Swagger
 var app = builder.Build();
 // Middlewares
 app.UseSwagger(); // Adiciona o Middleware do Swagger, gerando o documento para a TasksAPI (Normalmente usado em conjunto do UseSwaggerUI)
-app.UseSwaggerUI(); // Adiciona uma interface interativa do Swagger, permitindo visualizar melhor as rotas e endpoints
+app.UseSwaggerUI(); // Adiciona uma interface interativa do Swagger, permitindo visualizar melhor as rotas e endpoints na rota /swagger
 
 // Rotas
 app.MapGet("/", () => "Para acessar a API, você deve acessar '/api/<endpoint>'! Por exemplo, para acessar todas as tarefas, você pode utilizar o '/api/tasks'");
 
+TaskEndpoints.Map(app);
+
+/*
 app.MapGet("/api/tasks", () => {
     var listaTasks = TaskStore.taskList;
 
     return Results.Ok(listaTasks);
 });
+*/
 
 // Execução
 app.Run();
