5e405343acf4fa8c7b798185655de2bc06a2a47b
hav0kinho <ruallysonfelype@gmail.com>
2023-12-21 20:51:34 -0300

    Criando uma classe para conter os Endpoints das Tasks, tirando essa responsabilidade do Program.cs

--- Endpoints/TaskEndpoints.cs
+++ Endpoints/TaskEndpoints.cs
@@ -1,6 +1,20 @@
-﻿namespace TasksAPI.Endpoints;
+﻿using TasksAPI.Data;
+using TasksAPI.Models;
 
-public class TaskEndpoints
+namespace TasksAPI.Endpoints;
+
+public static class TaskEndpoints
 {
+    private static RouteGroupBuilder _routeGroup;
+    public static void Map(WebApplication app) // Adiciona os Endpoints configurados na classe1
+    {
+        _routeGroup = app.MapGroup("/api");
+        _routeGroup.MapGet("/tasks", GetTaskModels);
+    }
 
+    private static List<TaskModel> GetTaskModels()
+    {
+        var listaTasks = TaskStore.taskList;
+        return listaTasks;
+    }
 }
