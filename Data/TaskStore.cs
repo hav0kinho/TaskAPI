486f4631af3fde42e9013073b45e23228bb5bae1
hav0kinho <ruallysonfelype@gmail.com>
2023-12-22 00:06:35 -0300

    Criação do método de AddTask, que serve para adicionar novas tasks na lista

--- Data/TaskStore.cs
+++ Data/TaskStore.cs
@@ -4,9 +4,14 @@ namespace TasksAPI.Data;
 
 public static class TaskStore
 {
-    public static List<TaskModel> taskList = new List<TaskModel>
+    public static List<TaskModel> taskList {get;} = new List<TaskModel>
     {
         new TaskModel{Id=1, Title="Limpar a casa", IsCompleted=false},
         new TaskModel{Id=2, Title="Fazer comida", IsCompleted=true},
     };
+
+    public static void AddTask(TaskModel task){
+        taskList.Add(task);
+    }
+
 }
