5dc54c87359bb96e7ceaf130712ae7ad02b6e49f
hav0kinho <ruallysonfelype@gmail.com>
2023-12-22 18:37:38 -0300

    Configurando o AutoMapper e FluentValidation ao projeto, com o MappingConfig.cs

---
+++ MappingConfig.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using TasksAPI.Models;
+using TasksAPI.Models.DTO;
+
+namespace TasksAPI;
+
+public class MappingConfig : Profile
+{
+    public MappingConfig()
+    {
+        CreateMap<TaskModel, TaskDTO>().ReverseMap();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,24 @@
 // Deve ser uma API básica que permite que o usuário possa usar um CRUD de Tasks.
 using Swashbuckle.AspNetCore; // Biblioteca para Swagger
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using FluentValidation;
+
 
 using TasksAPI.Data;
 using TasksAPI.Models;
 using TasksAPI.Endpoints;
 using TasksAPI.Data;
-using Microsoft.EntityFrameworkCore;
+using TasksAPI;
+
 
 var builder = WebApplication.CreateBuilder(args);
 // Services
 builder.Services.AddEndpointsApiExplorer(); // Prepara o terreno para criação do Swagger e OpenAI (Deve ser usado junto com o AddSwaggerGen())
 builder.Services.AddSwaggerGen(); // Permite a geração do Swagger
+
+builder.Services.AddAutoMapper(typeof(MappingConfig)); // Adicionando o AutoMapper para aplicação
+builder.Services.AddValidatorsFromAssemblyContaining<Program>(); // Adicionando o FluentValidation na aplicação!
 
 builder.Services.AddDbContext<TaskDb>(opt => opt.UseInMemoryDatabase("TaskList")); //Usando o TaskDb como base, e nas opt, estamos configurando um Banco baseado no InMemory! Colocando o nome dele como sendo "TaskList"
 
