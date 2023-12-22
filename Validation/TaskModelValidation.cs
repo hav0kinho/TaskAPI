eadd3fd603012fb1e2a087ce590e1c7741b102e4
hav0kinho <ruallysonfelype@gmail.com>
2023-12-22 18:38:05 -0300

    Criação das validações do TaskDTO

---
+++ Validation/TaskModelValidation.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using TasksAPI.Models.DTO;
+
+namespace TasksAPI.Validation;
+
+public class TaskModelValidation : AbstractValidator<TaskDTO>
+{
+    public TaskModelValidation()
+    {
+        RuleFor(task => task.Title)
+        .NotEmpty()
+        .NotNull()
+        .Length(1, 100);
+        
+        RuleFor(task => task.IsCompleted).NotNull();
+    }
+}
