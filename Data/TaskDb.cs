c3ac64e186d0725f66446f050b6b5f20427c93e2
hav0kinho <ruallysonfelype@gmail.com>
2023-12-22 01:04:38 -0300

    Criando o TaskDb, que servirá como o Banco de dados da aplicação

---
+++ Data/TaskDb.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TasksAPI.Models;
+
+namespace TasksAPI.Data;
+
+public class TaskDb : DbContext
+{
+    public TaskDb(DbContextOptions<TaskDb> options) : base(options){}
+
+    public DbSet<TaskModel> Tasks => Set<TaskModel>(); // Criando a "Tabela" no Banco!
+}
