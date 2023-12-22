a6f4cfd511d9e6bed98028590888485aac02cb5a
hav0kinho <ruallysonfelype@gmail.com>
2023-12-22 00:10:45 -0300

    Criação dos endpoints: GetTask para um Id específico | PostTask para Post de novas Tasks

--- Endpoints/TaskEndpoints.cs
+++ Endpoints/TaskEndpoints.cs
@@ -6,15 +6,29 @@ namespace TasksAPI.Endpoints;
 public static class TaskEndpoints
 {
     private static RouteGroupBuilder _routeGroup;
-    public static void Map(WebApplication app) // Adiciona os Endpoints configurados na classe1
+    public static void Map(WebApplication app) // Adiciona os Endpoints configurados na classe
     {
         _routeGroup = app.MapGroup("/api");
-        _routeGroup.MapGet("/tasks", GetTaskModels);
+        _routeGroup.MapGet("/task", GetTasks);
+        _routeGroup.MapGet("/task/{id:int}", GetTask);
+        _routeGroup.MapPost("/task", PostTask);
     }
 
-    private static List<TaskModel> GetTaskModels()
+    private static IResult GetTasks()
     {
         var listaTasks = TaskStore.taskList;
-        return listaTasks;
+        return Results.Ok(listaTasks);
+    }
+
+    private static IResult GetTask(int id)
+    {
+        var taskRequisitada = TaskStore.taskList.FirstOrDefault(task => task.Id == id);
+        return Results.Ok(taskRequisitada);
+    }
+
+    private static IResult PostTask(TaskModel task)
+    {
+        TaskStore.AddTask(task);
+        return Results.Created($"/task/{task.Id}", task);
     }
 }
